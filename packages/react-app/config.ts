a2f384c9d9fc6c94675ac8091b305d179b7fbc92
fraolb <fraolbereket@gmail.com>
2024-04-15 23:06:03 +0300

    customize header

---
+++ packages/react-app/config.ts
@@ -0,0 +1,29 @@
+import { celo, celoAlfajores } from "wagmi/chains";
+import { http, WagmiProvider, createConfig } from "wagmi";
+import {
+  RainbowKitProvider,
+  connectorsForWallets,
+} from "@rainbow-me/rainbowkit";
+import { injectedWallet } from "@rainbow-me/rainbowkit/wallets";
+
+const connectors = connectorsForWallets(
+  [
+    {
+      groupName: "Recommended",
+      wallets: [injectedWallet],
+    },
+  ],
+  {
+    appName: "Celo Composer",
+    projectId: "044601f65212332475a09bc14ceb3c34",
+  }
+);
+
+export const config = createConfig({
+  connectors,
+  chains: [celo, celoAlfajores],
+  transports: {
+    [celo.id]: http(),
+    [celoAlfajores.id]: http(),
+  },
+});
